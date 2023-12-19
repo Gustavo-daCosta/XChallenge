020d288037b3d066c131f3ba2eff6db1d57342c7
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-12-19 12:03:36 -0300

    API: Repositories base iniciados

---
+++ xchallenge.api/Repositories/AcessoRepository.cs
@@ -0,0 +1,74 @@
+﻿using xchallenge.api.Contexts;
+using xchallenge.api.Domains;
+using xchallenge.api.Interfaces;
+
+namespace xchallenge.api.Repositories
+{
+    public class AcessoRepository : IAcessoRepository
+    {
+        private readonly XChallengeContext _context;
+
+        public AcessoRepository()
+        {
+            _context = new XChallengeContext();
+        }
+
+        public void Atualizar(int id, Acesso acesso)
+        {
+            try
+            {
+                Acesso acessoBuscado = _context.Acessos.Find(id)!;
+
+                if (acessoBuscado != null)
+                {
+                    acessoBuscado.Nome = acesso.Nome;
+                    acessoBuscado.Email = acesso.Email;
+                    acessoBuscado.SenhaAcesso = acesso.SenhaAcesso;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Acesso BuscarPorEmailSenha(string email, string senha)
+        {
+            try
+            {
+                Acesso acessoBuscado = _context.Acessos.FirstOrDefault(a => a.Email == email && a.SenhaAcesso == senha)!;
+                return acessoBuscado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Acesso BuscarPorId(int id)
+        {
+            try
+            {
+                Acesso acessoBuscado = _context.Acessos.FirstOrDefault(a => a.IdAcesso == id)!;
+                return acessoBuscado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Cadastrar(Acesso acesso)
+        {
+            try
+            {
+                _context.Acessos.Add(acesso);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
