83cb31e927bbca7ead9d6cd4a8fa55f6d0fc1b79
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2023-12-19 10:22:49 -0300

    API: Conexão com o BD e domains geradas

---
+++ xchallenge.api/Domains/Modalidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace xchallenge.api.Domains;
+
+public partial class Modalidade
+{
+    public string IdModalidade { get; set; } = null!;
+
+    public string? NomeModalidade { get; set; }
+
+    public string? DescModalidade { get; set; }
+
+    public virtual ICollection<Competidor> Competidors { get; set; } = new List<Competidor>();
+
+    public virtual ICollection<DataNacional> DataNacionals { get; set; } = new List<DataNacional>();
+
+    public virtual ICollection<SkillModalidade> SkillModalidades { get; set; } = new List<SkillModalidade>();
+}
